06b6505bb95c366cc92c0a0172cf69f13a19eeea
Umair Sarfraz <aquadestructor@icloud.com>
2020-12-02 13:31:08 +0500

    hotfix: append backend URL to fetch params & extract JSON from response correctly
    As backend URL is different from frontend URL, it needs to be explicitly defined furing network call through fetch.
    
    This commit also extracts the JSON correctly from the response object received from server.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,7 +6,8 @@
     let success = false;
     let data = null;
     const explorer = 'https://tangle-explorer.dag.sh/chrysalis/';
-    
+    const backendUrl = 'http://localhost:3000';
+
     function validate(event) {
         done = false;
         if ((address.length == 63 && address[3] == '1') || address.length == 64 && address[4] == '1' ) {
@@ -22,8 +23,8 @@
         }
         waiting = true;
 
-        const res = await fetch('/api?address=' + address);
-        data = await res.body.json();
+        const res = await fetch(`${backendUrl}/api?address=${address}`);
+        data = await res.json();
 
         success = (res.status == 200);
         done = true;
